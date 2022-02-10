89ed0f78607e8412ca7328f85bf92f8a35cafd27
Warsame Osman <osman18@my.yorku.ca>
2022-02-10 08:09:30 -0500

    created java files for bubble and selection sort

---
+++ DS-and-Algorithms/SelectionSort.java
@@ -0,0 +1,28 @@
+public class SelectionSort {
+
+    // Selection sort, compares values but sorts values starting from the beginning, the last value
+    // to be sorted in the last value in the array. opposed to bubble sort.
+    // we loop and keep the minindex i at all times... if minindex value is greater then the current value then we make the minindex value the smaller current value.
+    SelectionSort(int [] a) {
+        int minindex;
+        for (int i = 0; i < a.length - 2) {
+            minindex = i;
+            for(int j = i+1; j < a.length - 1) {
+                if(a[minindex] > a[j]) {
+                    minindex = j;
+
+                    int temp = a[minindex];
+                    a[minindex] = a[i];
+                    a[i] = temp;
+
+                }
+
+
+            }
+
+
+        }
+        return a;
+    }
+
+}
